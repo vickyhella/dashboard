172bcd18fbcd667e5fe8f0ecc3c426c75585290f
yuzeng <cridequeger@yahoo.com>
2022-09-27 15:28:55 +0800

    Harvester: GUI will work properly if crd missed.

--- pkg/harvester/config/harvester.js
+++ pkg/harvester/config/harvester.js
@@ -340,7 +340,6 @@ export function init($plugin, store) {
   });
 
   virtualType({
-    ifHaveType:    LOGGING.CLUSTER_FLOW,
     labelKey:     'harvester.logging.clusterFlow.label',
     name:         HCI.CLUSTER_FLOW,
     namespaced:   true,
@@ -363,7 +362,6 @@ export function init($plugin, store) {
   });
 
   virtualType({
-    ifHaveType:    LOGGING.CLUSTER_OUTPUT,
     labelKey:     'harvester.logging.clusterOutput.label',
     name:         HCI.CLUSTER_OUTPUT,
     namespaced:   true,
@@ -386,7 +384,6 @@ export function init($plugin, store) {
   });
 
   virtualType({
-    ifHaveType:    LOGGING.FLOW,
     labelKey:     'harvester.logging.flow.label',
     name:         HCI.FLOW,
     namespaced:   true,
@@ -409,7 +406,6 @@ export function init($plugin, store) {
   });
 
   virtualType({
-    ifHaveType:    LOGGING.OUTPUT,
     labelKey:     'harvester.logging.output.label',
     name:         HCI.OUTPUT,
     namespaced:   true,

61e9dd066cb4ed455bce03c94454dab9269d2de6
n313893254 <n313893254@126.com>
2022-12-08 15:33:14 +0800

    HARVESTER: add harvester CI && remove e2e test

--- shell/scripts/drone-build-pkg.sh
+++ shell/scripts/drone-build-pkg.sh
@@ -7,8 +7,12 @@ source scripts/version
 if [[ $COMMIT_BRANCH == "master" ]]; then
   VERSION="latest"
 else
-  VERSION=$(cd pkg/$1; node -p -e "require('./package.json').version")
+  VERSION=$COMMIT_BRANCH
 fi
+
+echo "DRONE_TAG: ${DRONE_TAG}"
+echo "GIT_TAG: ${GIT_TAG}"
+echo "TAG_VERSION: ${TAG_VERSION}"
 
 echo "Drone Build Args"
 echo "COMMIT: ${COMMIT}"
@@ -26,8 +30,12 @@ EXIT_CODE=$?
 export PKG_NAME=${1}-${VERSION}
 export PKG_TARBALL=${PKG_NAME}.tar.gz
 
+export PKG_TAG_VERSION=${1}-${TAG_VERSION}
+export PKG_TAG_TARBALL=${TAG_VERSION}.tar.gz
+
 echo "Drone Build Artefacts"
 echo "Package Directory: ${PKG_NAME}"
 echo "Package Tarball: ${PKG_TARBALL}"
+echo "Tag Package: ${PKG_TAG_VERSION} ${PKG_TAG_TARBALL}"
 
 exit $EXIT_CODE

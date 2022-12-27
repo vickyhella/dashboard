9d9e7d5bbe41ba753fe3cef564d219a6c123efd2
n313893254 <n313893254@126.com>
2022-12-27 18:17:13 +0800

    HARVESTER: Fix VM viewLogs action

--- pkg/harvester/models/kubevirt.io.virtualmachine.js
+++ pkg/harvester/models/kubevirt.io.virtualmachine.js
@@ -178,7 +178,7 @@ export default class VirtVm extends HarvesterResource {
       },
       {
         action:  'openLogs',
-        enabled: (this.podResource || []).length > 0,
+        enabled: !!this.podResource,
         icon:    'icon icon-fw icon-chevron-right',
         label:   this.t('harvester.action.viewlogs'),
         total:   1

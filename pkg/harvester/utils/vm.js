395628b771472cbfc3b007b19205794e90f26d14
n313893254 <n313893254@126.com>
2023-01-25 22:18:09 +0800

    HARVESTER: Fix build plugin error

--- pkg/harvester/utils/vm.js
+++ pkg/harvester/utils/vm.js
@@ -1,4 +1,4 @@
-import { HCI as HCI_ANNOTATIONS } from '@/pkg/harvester/config/labels-annotations';
+import { HCI as HCI_ANNOTATIONS } from '@pkg/harvester/config/labels-annotations';
 
 export function parseVolumeClaimTemplates(data) {
   const volumeClaimTemplateString = data?.metadata?.annotations[HCI_ANNOTATIONS.VOLUME_CLAIM_TEMPLATE];

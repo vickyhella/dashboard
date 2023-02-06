38649a6b5da37b558c125ca4caaf52dc933164b1
wujun <897415845@qq.com>
2023-02-06 11:19:37 +0800

    HARVESTER: fix message

--- pkg/harvester/validators/volume.js
+++ pkg/harvester/validators/volume.js
@@ -1,6 +1,6 @@
 export function volumeSize(size, getters, errors, validatorArgs, displayKey, value) {
   if (!/^([1-9][0-9]{0,8})[a-zA-Z]+$/.test(size)) {
-    const message = getters['i18n/t']('harvester.validation.vm.volume.maximumSize');
+    const message = getters['i18n/t']('harvester.validation.generic.maximumSize', { max: '999999999 GiB' });
 
     errors.push(message);
   }

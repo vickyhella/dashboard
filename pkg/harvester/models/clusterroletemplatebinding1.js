ef24ae33612ba346387a77ef52c02c62512900a7
wujun <897415845@qq.com>
2023-03-03 15:33:00 +0800

    HARVESTER: delete unuse model file
    (cherry picked from commit cdd6d58b46565268e38aca9029a638f24de992c5)

--- pkg/harvester/models/clusterroletemplatebinding1.js
+++
@@ -1,8 +0,0 @@
-import { MANAGEMENT } from '@shell/config/types';
-import NormanModel from '@shell/plugins/steve/norman-class';
-
-export default class CRTB extends NormanModel {
-  get clusterroletemplatebinding() {
-    return this.$rootGetters[`management/byId`](MANAGEMENT.CLUSTER_ROLE_TEMPLATE_BINDING, this.id?.replace(':', '/'));
-  }
-}
